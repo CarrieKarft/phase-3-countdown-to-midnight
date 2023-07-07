8f0a04de761b9af4071948a9a9f2e13149ca0467
Carrie Kraft <carriekarft@gmail.com>
2023-07-07 15:29:22 -0700

    commit tree 819aa14dc586865de05957a36710b88845c9bc68

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(int) 
+    while int >= 1
+        puts "#{int} SECOND(S)!"
+        int -=1
+    end
+    "HAPPY NEW YEAR!"
 end
+puts countdown(7)
+
+def countdown_with_sleep(int) 
+    while int >= 1
+        num_seconds = 1
+        puts "#{int} SECOND(S)!"
+        int -=1
+        sleep(num_seconds)
+    end
+    "HAPPY NEW YEAR!"
+end
+puts countdown_with_sleep(9)
